0f84a7352c72d4a02e3a447542aa80f999819cbc
Lippiece <lippiece@proton.me>
2022-12-28 12:12:17 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App () {
+  return (
+    <div className="App">
+      <p>
+        Edit <code>src/App.jsx</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
+
+// https://vitejs.dev/config/
+export default defineConfig( {
+  plugins: [
+    react(),
+    viteCompression(),
+  ],
+} );
